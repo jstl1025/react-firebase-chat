2b2843e2a2db847619c71ff6a0f4a20216d550d5
jstl1025 <jstl1025@gmail.com>
2024-10-16 22:20:22 -0600

    Changes up to add emoji picker

--- src/components/list/chatList/ChatList.jsx
+++ src/components/list/chatList/ChatList.jsx
@@ -11,6 +11,27 @@ const ChatList = () => {
             </div>
             <img src={addMode ? "./minus.png" : "./plus.png"} alt="" className="add" onClick={() => setAddMode((prev) => !prev)} />
         </div>
+        <div className="item">
+            <img src="./avatar.png" alt="" />
+            <div className="text">
+                <span>Jane Doe</span>
+                <p>Hello</p>
+            </div>
+        </div>
+        <div className="item">
+            <img src="./avatar.png" alt="" />
+            <div className="text">
+                <span>Jane Doe</span>
+                <p>Hello</p>
+            </div>
+        </div>
+        <div className="item">
+            <img src="./avatar.png" alt="" />
+            <div className="text">
+                <span>Jane Doe</span>
+                <p>Hello</p>
+            </div>
+        </div>
     </div>
 }
 
