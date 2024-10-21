86c8447b1fc163d75dcd9f82d307341d4d894b09
jstl1025 <jstl1025@gmail.com>
2024-10-20 21:56:00 -0600

    Changes up to chat message design

--- src/components/chat/Chat.jsx
+++ src/components/chat/Chat.jsx
@@ -24,7 +24,42 @@ const Chat = () => {
                 <img src="./info.png" alt="" />
             </div>
         </div>
-        <div className="center"></div>
+        <div className="center">
+            <div className="message">
+                <img src="./avatar.png" alt="" />
+                <div className="texts">
+                    <p>哈囉，你好嗎?哈囉，你好嗎?哈囉，你好嗎?</p>
+                    <span>1 min ago</span>
+                </div>
+            </div>
+            <div className="message own">
+                <div className="texts">
+                    <p>哈囉，我好嗎?哈囉，我好嗎?哈囉，我好嗎?</p>
+                    <span>1 min ago</span>
+                </div>
+            </div>
+            <div className="message">
+                <img src="./avatar.png" alt="" />
+                <div className="texts">
+                    <p>哈囉，你好嗎?哈囉，你好嗎?哈囉，你好嗎?</p>
+                    <span>1 min ago</span>
+                </div>
+            </div>
+            <div className="message own">
+                <div className="texts">
+                    <p>哈囉，我好嗎?哈囉，我好嗎?哈囉，我好嗎?</p>
+                    <span>1 min ago</span>
+                </div>
+            </div>
+            <div className="message">
+                <img src="./avatar.png" alt="" />
+                <div className="texts">
+                    <img src="https://i.natgeofe.com/k/6d301bfc-ff93-4f6f-9179-b1f66b19b9b3/pig-young-closeup_3x4.jpg" alt="" />
+                    <p>哈囉，你好嗎?哈囉，你好嗎?哈囉，你好嗎?</p>
+                    <span>1 min ago</span>
+                </div>
+            </div>
+        </div>
         <div className="bottom">
             <div className="icons">
                 <img src="./img.png" alt="" />
